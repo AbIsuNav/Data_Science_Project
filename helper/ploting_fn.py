68873246d9a3ae88743fd0cc0c7406218e5e948c
AlexLacson <alexander_nou@hotmail.com>
2019-11-15 17:10:34 +0100

    Added function for plotting heatmaps.

--- helper/ploting_fn.py
+++ helper/ploting_fn.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
-
+import cv2
 
 def plot_ROC(prediction, target, class_names, save=False):
     """
@@ -36,3 +36,25 @@ def plot_ROC(prediction, target, class_names, save=False):
     if save:
         plt.savefig("roc_curve.png")
     plt.show()
+
+def plot_heatmaps(image_batch, model, save_path='./figures/', resize_dim=(224, 224)):
+    """
+    Plots class activation maps for a batch of images.
+    :param image_batch: Batch of images, dimensions (Batch size, 3, H, W)
+    :param model: Trained model having option to compute CAM
+    :param save_path: Path to store figures of heatmaps
+    :param resize_dim: Dimension for upsampling of images
+    :return:
+    """
+    out = model(image_batch, CAM=True)
+    for i in range(len(out)):
+        for c in range(out[0].size(0)):
+            img = out[i][c].detach().numpy()
+
+            #plt.pcolormesh(cv2.resize(img, resize_dim))
+
+            plt.imshow(cv2.resize(img, resize_dim))
+            
+            plt.title('Activation map, sample {}, class {}'.format(i, c))
+            plt.savefig(f'{save_path}CAM_sample_{i}_class_{c}.png')
+            #plt.show()
